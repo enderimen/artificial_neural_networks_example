7892b39b4becc922ea38d6470c95194f43b044cd
Ender İMEN <enderimen@hotmail.com>
2018-03-18 23:57:08 +0300

    "dataset" class file created.

---
+++ artificial_neural_networks/artificial_neural_networks/data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace artificial_neural_networks
+{
+    class data:activation_functions
+    {
+        private int agirlik;
+
+        public void setAgirlik(int agirlik)
+        {
+            this.agirlik = agirlik;
+        }
+
+        public int getAgirlik()
+        {
+            return agirlik;
+        }
+
+        double activation_functions.sigmoid(int x)
+        {
+            double ust = Math.Pow(Math.E, -x);
+            double F = 1 / (1 + ust);
+
+            return F;
+        }
+    }
+}

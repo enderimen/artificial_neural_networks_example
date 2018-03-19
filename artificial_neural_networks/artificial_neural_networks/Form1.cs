00a0fb11bec7a559d1de4285fd9e1a0bc91f37ee
Ender İMEN <enderimen@hotmail.com>
2018-03-19 16:56:03 +0300

    the project has been redesigned with object-oriented programming.

--- artificial_neural_networks/artificial_neural_networks/Form1.cs
+++ artificial_neural_networks/artificial_neural_networks/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,59 @@ namespace artificial_neural_networks
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void hesapla()
+        {
+            
+            data data1 = new data();
+
+            int parametre_sayisi = 5;
+
+            decimal alt = alt_sinir.Value;
+            decimal ust = ust_sinir.Value;
+
+            data1.setGirdiler(parametre_sayisi, (int)alt, (int)ust);
+
+            
+            if (data1.I_normal_toplama(toplama_fonk.Text, parametre_sayisi) != 1)
+            {
+                label6.Text += data1.I_normal_toplama(toplama_fonk.Text, parametre_sayisi);
+            }
+            else
+            {
+                Console.Beep();
+                MessageBox.Show("Toplama işlemi tanımlanamadı.");
+                label6.Text += 0;
+            }
+
+
+            label7.Text += data1.sigmoid(aktivasyon_fonk.Text);
+
+            double[,] gelen_girdiler = data1.getGirdiler();
+
+            for (int i = 0; i < parametre_sayisi; i++)
+            {
+                   listBox1.Items.Add(gelen_girdiler[i, 0]);
+                   listBox2.Items.Add(gelen_girdiler[i, 1]);
+            }
+        }
+
+        private void hesapla_buton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            if (parametre_sayisi.Text != null && ust_sinir.Value != 0 && alt_sinir.Value != 0 && toplama_fonk.Text != ""  && aktivasyon_fonk.Text != "")
+            {
+                if (alt_sinir.Value < ust_sinir.Value)
+                {
+                    hesapla();
+                }
+                else
+                {
+                    MessageBox.Show("Alt sınır üst sınırdan büyük olamaz");
+                }
+            }            
         }
     }
 }
--- artificial_neural_networks/artificial_neural_networks/data.cs
+++ artificial_neural_networks/artificial_neural_networks/data.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace artificial_neural_networks
 {
-    class data:activation_functions
+    class data:activation_functions,sum_functions
     {
         private int agirlik;
+        private double[,] girdiler;
+
+        double toplam = 0.0;
+        Random agirlik_uret = new Random();
+        Random bagimsiz_degiskenler = new Random();  
 
         public void setAgirlik(int agirlik)
         {
@@ -20,12 +25,80 @@ namespace artificial_neural_networks
             return agirlik;
         }
 
-        double activation_functions.sigmoid(int x)
+        public void setGirdiler(int parametreSayisi,int alt_sinir, int ust_sinir)
         {
-            double ust = Math.Pow(Math.E, -x);
-            double F = 1 / (1 + ust);
+            girdiler = new double[parametreSayisi, 2];
 
+            for (int i = 0; i < parametreSayisi; i++)
+            {
+                int b = bagimsiz_degiskenler.Next(alt_sinir,ust_sinir);
+
+                for (int k = 0; k < 2; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        girdiler[i, k] = b;
+                        //listBox1.Items.Add(girdi[i, k]);
+                    }
+                    else
+                    {
+                        girdiler[i, k] = agirlik_uret.NextDouble();
+                        //listBox2.Items.Add(girdi[i, k]);
+                    }
+                }
+            }
+        }
+
+        public double[,] getGirdiler(){
+            return girdiler;
+        }
+
+        // sigmoid metodu :interface
+        public double sigmoid(string activation_function)
+        {
+            double F = 0.0;
+            switch (activation_function)
+            {
+                case "Sigmoid":
+
+                    double ust_ifade = Math.Pow(Math.E, -toplam);
+                    F = 1 / (1 + ust_ifade);
+
+                    break;
+                default:
+                    break;
+            }
             return F;
+        }
+
+
+        // normal toplama metodu :interface
+        public double I_normal_toplama(string toplama_fonk, int parametre_sayisi)
+        {
+            bool error = false;
+            switch (toplama_fonk)
+            {
+                case "∑":
+
+                    for (int j = 0; j < parametre_sayisi; j++)
+                    {
+                        toplam += girdiler[j, 0] * girdiler[j, 1];
+                    }
+                    break;
+                case "a":
+                    error = true;
+
+                    //MessageBox.Show("İşlem tanımlanmadı");
+                    break;
+                default:
+                    break;
+            }
+
+            if (error==true)
+            {
+                return 1;
+            }
+            return toplam;
         }
     }
 }

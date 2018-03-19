7fc866141fef7d98e304dda397c7e4005cfeed1f
Ender İMEN <enderimen@hotmail.com>
2018-03-19 16:53:54 +0300

    new components added to ui

--- artificial_neural_networks/artificial_neural_networks/Form1.Designer.cs
+++ artificial_neural_networks/artificial_neural_networks/Form1.Designer.cs
@@ -39,18 +39,21 @@
             this.label5 = new System.Windows.Forms.Label();
             this.aktivasyon_fonk = new System.Windows.Forms.ComboBox();
             this.hesapla_buton = new System.Windows.Forms.Button();
-            this.data_set = new System.Windows.Forms.DataGridView();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.alt_sinir)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ust_sinir)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.data_set)).BeginInit();
             this.SuspendLayout();
             // 
             // parametre_sayisi
             // 
             this.parametre_sayisi.Location = new System.Drawing.Point(31, 43);
             this.parametre_sayisi.Name = "parametre_sayisi";
-            this.parametre_sayisi.Size = new System.Drawing.Size(100, 20);
+            this.parametre_sayisi.Size = new System.Drawing.Size(108, 20);
             this.parametre_sayisi.TabIndex = 0;
             // 
             // label1
@@ -58,9 +61,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(28, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Parametre Sayısı";
+            this.label1.Text = "Bağımsız Değiş. Sayısı";
             // 
             // label2
             // 
@@ -82,14 +85,14 @@
             // 
             // alt_sinir
             // 
-            this.alt_sinir.Location = new System.Drawing.Point(76, 87);
+            this.alt_sinir.Location = new System.Drawing.Point(84, 84);
             this.alt_sinir.Name = "alt_sinir";
             this.alt_sinir.Size = new System.Drawing.Size(55, 20);
             this.alt_sinir.TabIndex = 2;
             // 
             // ust_sinir
             // 
-            this.ust_sinir.Location = new System.Drawing.Point(76, 113);
+            this.ust_sinir.Location = new System.Drawing.Point(84, 110);
             this.ust_sinir.Name = "ust_sinir";
             this.ust_sinir.Size = new System.Drawing.Size(55, 20);
             this.ust_sinir.TabIndex = 2;
@@ -99,8 +102,9 @@
             this.toplama_fonk.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.toplama_fonk.FormattingEnabled = true;
             this.toplama_fonk.Items.AddRange(new object[] {
-            "∑"});
-            this.toplama_fonk.Location = new System.Drawing.Point(182, 42);
+            "∑",
+            "a"});
+            this.toplama_fonk.Location = new System.Drawing.Point(222, 42);
             this.toplama_fonk.Name = "toplama_fonk";
             this.toplama_fonk.Size = new System.Drawing.Size(121, 21);
             this.toplama_fonk.TabIndex = 3;
@@ -108,7 +112,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(179, 26);
+            this.label4.Location = new System.Drawing.Point(219, 26);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(115, 13);
             this.label4.TabIndex = 1;
@@ -117,7 +121,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(179, 93);
+            this.label5.Location = new System.Drawing.Point(219, 93);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(126, 13);
             this.label5.TabIndex = 1;
@@ -129,14 +133,14 @@
             this.aktivasyon_fonk.FormattingEnabled = true;
             this.aktivasyon_fonk.Items.AddRange(new object[] {
             "Sigmoid"});
-            this.aktivasyon_fonk.Location = new System.Drawing.Point(182, 109);
+            this.aktivasyon_fonk.Location = new System.Drawing.Point(222, 109);
             this.aktivasyon_fonk.Name = "aktivasyon_fonk";
             this.aktivasyon_fonk.Size = new System.Drawing.Size(121, 21);
             this.aktivasyon_fonk.TabIndex = 3;
             // 
             // hesapla_buton
             // 
-            this.hesapla_buton.Location = new System.Drawing.Point(359, 26);
+            this.hesapla_buton.Location = new System.Drawing.Point(372, 298);
             this.hesapla_buton.Name = "hesapla_buton";
             this.hesapla_buton.Size = new System.Drawing.Size(123, 40);
             this.hesapla_buton.TabIndex = 4;
@@ -144,30 +148,69 @@
             this.hesapla_buton.UseVisualStyleBackColor = true;
             this.hesapla_buton.Click += new System.EventHandler(this.hesapla_buton_Click);
             // 
-            // data_set
-            // 
-            this.data_set.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.data_set.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.data_set.Location = new System.Drawing.Point(0, 348);
-            this.data_set.Name = "data_set";
-            this.data_set.Size = new System.Drawing.Size(519, 111);
-            this.data_set.TabIndex = 5;
-            // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(31, 151);
+            this.listBox1.Location = new System.Drawing.Point(31, 165);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(100, 173);
+            this.listBox1.Size = new System.Drawing.Size(64, 173);
             this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(94, 165);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(105, 173);
+            this.listBox2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(219, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Toplama fonk. sonucu: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(219, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Aktivasyon fonk. sonucu:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Parametreler";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(93, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Ağırlık Değerleri";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 459);
+            this.ClientSize = new System.Drawing.Size(507, 350);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.data_set);
             this.Controls.Add(this.hesapla_buton);
             this.Controls.Add(this.aktivasyon_fonk);
             this.Controls.Add(this.toplama_fonk);
@@ -186,7 +229,6 @@
             this.Text = "Artificial Neural Networks Dataset Processing";
             ((System.ComponentModel.ISupportInitialize)(this.alt_sinir)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ust_sinir)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.data_set)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,8 +247,12 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox aktivasyon_fonk;
         private System.Windows.Forms.Button hesapla_buton;
-        private System.Windows.Forms.DataGridView data_set;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
